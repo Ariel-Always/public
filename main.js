ee3e159d9ff5393e410a81f02a76fa799a64c010
Dogelans MikeAser <zhangyangllx@163.com>
2017-04-26 15:07:50 +0800

    增加map，forEach,filter,some,every,indexOf,lastIndexof,reduce,reduceRight的低版本IE兼容性

--- main.js
+++ main.js
@@ -105,17 +105,126 @@ function setCookie(val,time){
 //}
 
 
-Array.prototype.myForEach = function myForEach(callBack, context) {					//让forEach兼容IE浏览器
-    typeof context === "undefined" ? context = window : null;
-    if ("forEach" in Array.prototype) {
-        this.forEach(callBack, context);
-        return;
-    }
 
-    //->不兼容处理
-    for (var i = 0; i < this.length; i++) {
-        typeof callBack === "function" ? callBack.call(context, this[i], i, this) : null;
-    }
-};
 
+if (typeof Array.prototype.forEach != "function") {									//forEach兼容IE
+	  Array.prototype.forEach = function (fn, context) {
+	    for (var k = 0, length = this.length; k < length; k++) {
+	      if (typeof fn === "function" && Object.prototype.hasOwnProperty.call(this, k)) fn.call(context, this[k], k, this);
+	    }
+	  };
+	}
+
+	if (typeof Array.prototype.map != "function") {									//map兼容IE
+	  Array.prototype.map = function (fn, context) {
+		var arr = [];
+		if (typeof fn === "function") {
+	   	  for (var k = 0, length = this.length; k < length; k++) {
+			  arr.push(fn.call(context, this[k], k, this));
+		  }
+	    }
+		return arr;
+	  };
+	}
+
+	if (typeof Array.prototype.filter != "function") {								//filter兼容IE
+	  Array.prototype.filter = function (fn, context) {
+		var arr = [];
+		if (typeof fn === "function") {
+	   	  for (var k = 0, length = this.length; k < length; k++) {
+			  fn.call(context, this[k], k, this) && arr.push(this[k]);
+		  }
+	    }
+		return arr;
+	  };
+	}
+
+	if (typeof Array.prototype.some != "function") {								//some兼容IE
+	  Array.prototype.some = function (fn, context) {
+		var passed = false;
+		if (typeof fn === "function") {
+	   	  for (var k = 0, length = this.length; k < length; k++) {
+			  if (passed === true) break;
+			  passed = !!fn.call(context, this[k], k, this);
+		  }
+	    }
+		return passed;
+	  };
+	}
+
+	if (typeof Array.prototype.every != "function") {							//every兼容IE
+	  Array.prototype.every = function (fn, context) {
+		var passed = true;
+		if (typeof fn === "function") {
+	   	  for (var k = 0, length = this.length; k < length; k++) {
+			  if (passed === false) break;
+			  passed = !!fn.call(context, this[k], k, this);
+		  }
+	    }
+		return passed;
+	  };
+	}
+
+	if (typeof Array.prototype.indexOf != "function") {							//indexof兼容IE
+	  Array.prototype.indexOf = function (searchElement, fromIndex) {
+		var index = -1;
+		fromIndex = fromIndex * 1 || 0;
+
+	   	for (var k = 0, length = this.length; k < length; k++) {
+		  if (k >= fromIndex && this[k] === searchElement) {
+			  index = k;
+			  break;
+		  }
+	    }
+		return index;
+	  };
+	}
+
+	if (typeof Array.prototype.lastIndexOf != "function") {						
+	  Array.prototype.lastIndexOf = function (searchElement, fromIndex) {
+		var index = -1, length = this.length;
+		fromIndex = fromIndex * 1 || length - 1;
+
+	   	for (var k = length - 1; k > -1; k-=1) {
+	    	if (k <= fromIndex && this[k] === searchElement) {
+				index = k;
+				break;
+			}
+	    }
+		return index;
+	  };
+	}
+
+	if (typeof Array.prototype.reduce != "function") {								//reduce兼容IE
+	  Array.prototype.reduce = function (callback, initialValue ) {
+		 var previous = initialValue, k = 0, length = this.length;
+		 if (typeof initialValue === "undefined") {
+			previous = this[0];
+			k = 1;
+		 }
+		 
+	    if (typeof callback === "function") {
+	      for (k; k < length; k++) {
+			 this.hasOwnProperty(k) && (previous = callback(previous, this[k], k, this));
+		  }
+	    }
+		return previous;
+	  };
+	}
+
+	if (typeof Array.prototype.reduceRight != "function") {
+	  Array.prototype.reduceRight = function (callback, initialValue ) {
+	    var length = this.length, k = length - 1, previous = initialValue;
+	    if (typeof initialValue === "undefined") {
+	        previous = this[length - 1];
+	        k--;
+	    }
+	    if (typeof callback === "function") {
+	       for (k; k > -1; k-=1) {          
+	          this.hasOwnProperty(k) && (previous = callback(previous, this[k], k, this));
+	       }
+	    }
+	    return previous;
+	  };
+	}
 
